d81b9605195d8faca7452051911d5e66db2788ab
parthsujalshah <parthsujalshah@gmail.com>
2021-09-18 13:46:26 +0530

    image change

--- generators/constants/constants.py
+++ generators/constants/constants.py
@@ -1,5 +1,5 @@
 image_list = [
     'https://edit.org/images/cat/book-covers-big-2019101610.jpg',
     'https://binaries.templates.cdn.office.net/support/templates/en-us/lt22301254_quantized.png',
-    'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/action-thriller-book-cover-design-template-3675ae3e3ac7ee095fc793ab61b812cc_screen.jpg?ts=1588152105',
+    'https://m.media-amazon.com/images/I/41gr3r3FSWL.jpg',
 ]
